6118aba2a5bc19e856dba6d9224c841d256e1f34
tsheeran1 <tom.sheeran@me.com>
2020-03-24 18:46:24 -0400

    more tests

--- scripts/environment-setup.sh
+++ scripts/environment-setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "Starting Environment Setup"
+
 echo "##vso[task.setvariable variable=AZURE_CLIENT_ID;issecret=true]${servicePrincipalId}"
 echo "##vso[task.setvariable variable=AZURE_CLIENT_SECRET;issecret=true]${servicePrincipalKey}"
 echo "##vso[task.setvariable variable=AZURE_SUBSCRIPTION_ID;issecret=true]$(az account show --query 'id' -o tsv)"
