f36c258139fe4da05418f1059fac4d6311efeaee
tsheeran1 <tom.sheeran@me.com>
2020-03-26 18:46:38 -0400

    further explore terraform init

--- scripts/terraform-init.sh
+++ scripts/terraform-init.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "Starting Terraform-init.sh"
+echo "argument passed = ${1}"
+echo "WORKING_DIRECTORY = ${WORKING_DIRECTORY}"
+
+
 WORKING_DIRECTORY=${1}
 cd ${WORKING_DIRECTORY}
 
