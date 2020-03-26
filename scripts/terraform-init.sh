159e161c26b84f995ebd7d71646b4950841c9718
tsheeran1 <tom.sheeran@me.com>
2020-03-26 18:59:23 -0400

    learning linux

--- scripts/terraform-init.sh
+++ scripts/terraform-init.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 echo "Starting Terraform-init.sh"
 echo "argument passed = ${1}"
 
-echo "before pwd = ${pwd}"
+echo "before pwd = $PWD"
 WORKING_DIRECTORY=${1}
 echo "WORKING_DIRECTORY = ${WORKING_DIRECTORY}"
 cd ${WORKING_DIRECTORY}
-echo "new pwd = ${pwd}"
+echo "new pwd = $PWD"
 terraform init -input=false 
