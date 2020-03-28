916327fb6fd9a2400d90fbd881e6e82b2400c6a0
tsheeran1 <tom.sheeran@me.com>
2020-03-28 11:21:31 -0400

    restored backend

--- versions.tf
+++ versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = ">= 0.12"
   backend "azurerm" {
-    resource_group_name  = "terraformrg"
-    storage_account_name = "tsheerantfstate"
+    resource_group_name  = "az-devops-terraform-rg"
+    storage_account_name = "aztfbackend"
     container_name       = "terraform"
     key                  = "terraform-getting-started.tfstate"
   }
